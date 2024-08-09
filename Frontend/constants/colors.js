2a444b8af69296fad8727a86ebfda94bdcfdcbab
Pabani401 <150105845+Pabani401@users.noreply.github.com>
2024-08-09 14:17:36 +0530

    Add images

--- Frontend/constants/colors.js
+++ Frontend/constants/colors.js
@@ -1,9 +1,35 @@
-const COLORS={
-    white:"#FFFFFF",
-    black:"#222222",
-    primary:"#007260",
-    secondary:"#39B68D",
-    grey:"#CCCCCC"
-}
+import { Dimensions } from "react-native";
+const { height, width } = Dimensions.get('window');
+
+export const COLORS = {
+    white: "#FFFFFF",
+    black: "#222222",
+    primary: "#7C9A92",
+    secondary: "#39B68D",
+    grey: "#BEC2C2",
+    background: "#253334"
+};
+
+export const SIZES = {
+    base: 8,
+    font: 14,
+    radius: 30,
+    padding: 8,
+    padding2: 12,
+    padding3: 16,
+
+    largeTitle: 50,
+    h1: 30,
+    h2: 22,
+    h3: 20,
+    h4: 18,
+    body1: 30,
+    body2: 20,
+    body3: 16,
+    body4: 14,
+
+    width,  // Corrected typo
+    height,
+};
 
 export default COLORS;
---
+++ Frontend/constants/fonts.js
@@ -0,0 +1,7 @@
+export const FONTS={
+    balck:require("../assets/fonts/AlegreyaSans-Black.ttf"),
+    bold:require("../assets/fonts/AlegreyaSans-Bold.ttf"),
+    regular:require("../assets/fonts/AlegreyaSans-Regular.ttf"),
+    medium:require("../assets/fonts/AlegreyaSans-Medium.ttf"),
+    light:require("../assets/fonts/AlegreyaSans-Light.ttf"),
+}
