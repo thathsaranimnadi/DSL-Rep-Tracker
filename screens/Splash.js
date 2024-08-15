3a9855a0dcce3da80e93a830af025558681034de
Pabani Hansamali <pabani.20222417@iit.ac.lk>
2024-08-15 11:59:22 +0530

    Change the splash screen view

--- screens/Splash.js
+++ screens/Splash.js
@@ -1,36 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
-import * as Animatable from 'react-native-animatable'
+import { StyleSheet, View, Image } from 'react-native';
+import React, { useEffect } from 'react';
+import * as Animatable from 'react-native-animatable';
 
 // Define black color manually
 const black = "#000000";
 
 const Splash = ({ navigation }) => {
-   useEffect(()=> {
-        setTimeout(()=>{
-            navigation.navigate("Welcome");
-        },2000);
+   useEffect(() => {
+      setTimeout(() => {
+         navigation.navigate("Welcome");
+      }, 2000);
    }, []);
 
   return (
-    <View style={{
-        backgroundColor:"white",
-        flex:1,
-        justifyContent:"center",
-        alignItems:"center"
-        }}>
-
-      <Animatable.Text style={{
-        color:black,
-        fontSize:36,
-        fontWeight:"800"
-        }} duration={2000} animation="fadeInDownBig">
-        DSL
-      </Animatable.Text>
+    <View style={styles.container}>
+      <Animatable.Image
+        source={require('../assets/dsl.png')} 
+        style={styles.logo}
+        duration={2000}
+        animation="fadeInDownBig"
+      />
     </View>
-  )
+  );
 };
 
-export default Splash
+export default Splash;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    width: 150, 
+    height: 150, 
+    tintColor: black, 
+  },
+});
