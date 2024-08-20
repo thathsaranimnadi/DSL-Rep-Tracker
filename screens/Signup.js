9480b680782a84430102f0d22b7ad9761525cdd7
thathsaranimnadi <115542460+thathsaranimnadi@users.noreply.github.com>
2024-08-20 15:26:57 +0530

    import firestore

--- screens/Signup.js
+++ screens/Signup.js
@@ -5,10 +5,8 @@ import { RadioButton } from 'react-native-paper';
 import COLORS from '../constants/colors';
 import Button from '../components/Button';
 import app from '../firebaseConfig';
-
-
-
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getFirestore} from "firebase/firestore";
 
 const Signup = ({ navigation }) => {
   const [name, setName] = useState('');
